8befcc86589d1a36c5220c6e8f59d943e4a613da
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-01 23:50:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, surprise) {
+    for (let i = 0; i < names.length; i++) {
+      names.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return names;
+  }
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+countDown(10);
+
